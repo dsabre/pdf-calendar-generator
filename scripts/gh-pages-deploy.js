aa4098e81c72576b17d06e912e604c896df85ac5
Daniele Sabre <daniele.sabre@jakala.com>
2021-11-03 02:22:34 +0100

    Develop

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -5,7 +5,7 @@ const chalk      = require("chalk");
 const PAGES_BRANCH_NAME = 'gh-pages';
 const MAIN_BRANCH_NAME  = 'main';
 
-const runCommand = command => {
+const execSyncVerbose = command => {
     console.log(command);
     execSync(command);
 };
@@ -14,27 +14,27 @@ try {
     console.log(chalk.cyan('Deploy started'));
 
     console.log('\n' + chalk.cyan('Committing uncommitted changes'));
-    runCommand('git add .');
-    runCommand('git commit -m "Develop"');
-    runCommand('git pull');
-    runCommand('git push');
+    execSyncVerbose('git add .');
+    execSyncVerbose('git commit -m "Develop"');
+    execSyncVerbose('git pull');
+    execSyncVerbose('git push');
 
     console.log('\n' + chalk.cyan('Build project'));
-    runCommand(`git checkout --orphan ${PAGES_BRANCH_NAME}`);
-    runCommand('npm run build');
+    execSyncVerbose(`git checkout --orphan ${PAGES_BRANCH_NAME}`);
+    execSyncVerbose('npm run build');
 
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
     console.log('\n' + chalk.cyan(`Commit ${folderName} directory`));
-    runCommand(`git --work-tree ${folderName} add --all`);
-    runCommand(`git --work-tree ${folderName} commit -m "GitHub pages deploy"`);
+    execSyncVerbose(`git --work-tree ${folderName} add --all`);
+    execSyncVerbose(`git --work-tree ${folderName} commit -m "GitHub pages deploy"`);
 
     console.log('\n' + chalk.cyan(`Pushing to ${PAGES_BRANCH_NAME}`));
-    runCommand(`git push origin HEAD:${PAGES_BRANCH_NAME} --force`);
+    execSyncVerbose(`git push origin HEAD:${PAGES_BRANCH_NAME} --force`);
 
     console.log('\n' + chalk.cyan(`Return to ${MAIN_BRANCH_NAME} branch`));
-    runCommand(`rm -rf ${folderName}`);
-    runCommand(`git checkout -f ${MAIN_BRANCH_NAME}`);
-    runCommand(`git branch -D ${PAGES_BRANCH_NAME}`);
+    execSyncVerbose(`rm -rf ${folderName}`);
+    execSyncVerbose(`git checkout -f ${MAIN_BRANCH_NAME}`);
+    execSyncVerbose(`git branch -D ${PAGES_BRANCH_NAME}`);
 
     console.log('\n' + chalk.green('SUCCESSFULLY DEPLOYED'));
 } catch (err) {
