52b9a7a88a691de7aed56271ffc82ddf8552c89a
Daniele Sabre <daniele.sabre@jakala.com>
2021-11-03 02:19:22 +0100

    Develop

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -11,24 +11,26 @@ const runCommand = command => {
 };
 
 try {
-    console.log(chalk.cyan('Deploy started...'));
+    console.log(chalk.cyan('Deploy started'));
 
+    console.log('\n' + chalk.cyan('Build project'));
     runCommand(`git checkout --orphan ${PAGES_BRANCH_NAME}`);
     runCommand('npm run build');
 
     const folderName = fs.existsSync('dist') ? 'dist' : 'build';
-
+    console.log('\n' + chalk.cyan(`Commit ${folderName} directory`));
     runCommand(`git --work-tree ${folderName} add --all`);
     runCommand(`git --work-tree ${folderName} commit -m "GitHub pages deploy"`);
 
-    console.log(chalk.cyan(`Pushing to ${PAGES_BRANCH_NAME}...`));
-
+    console.log('\n' + chalk.cyan(`Pushing to ${PAGES_BRANCH_NAME}`));
     runCommand(`git push origin HEAD:${PAGES_BRANCH_NAME} --force`);
+
+    console.log('\n' + chalk.cyan(`Return to ${MAIN_BRANCH_NAME} branch`));
     runCommand(`rm -rf ${folderName}`);
     runCommand(`git checkout -f ${MAIN_BRANCH_NAME}`);
     runCommand(`git branch -D ${PAGES_BRANCH_NAME}`);
 
-    console.log(chalk.green('SUCCESSFULLY DEPLOYED'));
+    console.log('\n' + chalk.green('SUCCESSFULLY DEPLOYED'));
 } catch (err) {
     console.log(chalk.red(err.message));
     process.exit(1);
